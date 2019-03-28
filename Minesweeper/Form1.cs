51f9017f32e83584d4dfe03ec4c501b76a9e6b68
Feriixu <feriixu@users.noreply.github.com>
2019-03-28 13:25:08 +0100

    Initial commit

---
+++ Minesweeper/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Minesweeper
+{
+    public partial class Form1 : Form
+    {
+        private MineButton[] mineButtons;
+        private int[] positions = { -1, -11, -10, -9, 1, 9, 10, 11 };
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            mineButtons = new MineButton[100];
+            Random r = new Random();
+            for (int i = 0; i < 100; i++)
+            {
+                MineButton button = new MineButton
+                {
+                    IsBomb = (r.Next(0, 4) == 0) ? true : false,
+                    Num = i,
+                };
+                button.RaiseCustomEvent += CustomClick;
+                button.Text = button.IsBomb ? "" : "";
+                mineButtons[i] = button;
+            }
+
+            foreach (MineButton button in mineButtons)
+            {
+                tableLayoutPanelButtons.Controls.Add(button);
+            }
+
+        }
+
+        private void CustomClick(object sender, CustomEventArgs e)
+        {
+            if (mineButtons[e.Num].IsBomb)
+            {
+                MessageBox.Show("Verloren!");
+            }
+            else
+            {
+                mineButtons[e.Num].Enabled = false;
+                mineButtons[e.Num].FlatStyle = FlatStyle.Popup;
+                mineButtons[e.Num].BackColor = Color.Gray;
+                int count = 0;
+                foreach (int position in positions)
+                {
+                    int checkPos = e.Num + position;
+                    if (checkPos > 0 && checkPos < 99)
+                    {
+                        if (mineButtons[checkPos].IsBomb)
+                            count++;
+                    }
+                }
+
+                mineButtons[e.Num].Text = count.ToString();
+            }
+        }
+    }
+}
---
+++ Minesweeper/MineButton.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Minesweeper
+{ 
+    public class CustomEventArgs
+    {
+        public CustomEventArgs(int n)
+        {
+            num = n;
+        }
+        private int num;
+        public int Num
+        {
+            get { return num; }
+        }
+    }
+
+    class MineButton : System.Windows.Forms.Button
+    {
+        private bool isBomb;
+        private int nearby;
+        private int num;
+        public delegate void ButtonClickDelegate(object sender, CustomEventArgs e);
+        public event ButtonClickDelegate RaiseCustomEvent;
+
+        public bool IsBomb { get => isBomb; set => isBomb = value; }
+        public int Nearby { get => nearby; set => nearby = value; }
+        public int Num { get => num; set => num = value; }
+
+        public MineButton()
+        {
+            this.Size = new System.Drawing.Size(40, 40);
+            this.Click += buttonClick;
+            this.BackColor = Color.LightGray;
+            this.TabStop = false;
+        }
+
+        private void buttonClick(object sender, EventArgs e)
+        {
+            ButtonClickDelegate handler = RaiseCustomEvent;
+            if (handler != null)
+            {
+                handler(this, new CustomEventArgs(this.num));
+            }
+        }
+    }
+}
